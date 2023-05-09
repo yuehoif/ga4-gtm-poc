6051404f1a21e22256a6b4bc14b96ce5fe6ef511
Sunny Fung <yuehoif@gmail.com>
2023-05-09 09:51:52 +0800

    feat: imp mock devent

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 console.log("Hello world")
 console.log(dataLayer)
+
+const btn = document.querySelector(".btn");
+btn.addEventListener('click', () => {
+    dataLayer.push({
+        event: "GTMEvent",
+        eventCategory: "User Authentication",
+        eventAction: "Click",
+        eventLabel: "ExpressLogin_EN_Email"
+    })
+    console.log('push event')
+})
